f5baeccaaa6fbe998ef428b6ed7aaff595dfda0d
Adria2208 <adrialol68@gmail.com>
2026-02-01 18:10:05 +0100

    feat: implement final core gameplay mechanics - Player improvements - Dynamic world text - Music and sound effects

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,7 +4,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
+	Engine.time_scale = 0.5 # Slow down game speed.
+	body.get_node("CollisionShape2D").queue_free() # Remove that node.
 	timer.start()
 
 func _on_timer_timeout() -> void: # Execute when timer ends.
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
