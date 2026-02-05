28fb2c21456e7f8cdc59c4abf7bdf04bc8648d5e
Adrià Sánchez López <57090378+Adria2208@users.noreply.github.com>
2026-02-05 20:41:47 +0100

    Add mock settings menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,21 +1,27 @@
 extends Node2D
 
+@onready var camera_2d: Camera2D = $Camera2D
+@onready var options_menu: VBoxContainer = $OptionsMenu
+@onready var v_box_container: VBoxContainer = $VBoxContainer
+
+var camera_original_position: Vector2 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	camera_original_position = camera_2d.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_options_pressed() -> void:
-	print("Options button pressed.")
+	camera_2d.position.x = options_menu.position.x + (options_menu.size.x / 2)
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_back_options_button_pressed() -> void:
+	camera_2d.position = camera_original_position
