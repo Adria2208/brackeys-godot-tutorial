8619da6006acbd155c5f2894f737c243c1ed4586
Adrià Sánchez López <adrialol68@gmail.com>
2026-02-05 21:44:05 +0100

    Improve MainMenu animation

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,27 +1,15 @@
 extends Node2D
 
-@onready var camera_2d: Camera2D = $Camera2D
-@onready var options_menu: VBoxContainer = $OptionsMenu
-@onready var v_box_container: VBoxContainer = $VBoxContainer
-
-var camera_original_position: Vector2 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	camera_original_position = camera_2d.position
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@onready var animation_player: AnimationPlayer = $UI/AnimationPlayer
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_options_pressed() -> void:
-	camera_2d.position.x = options_menu.position.x + (options_menu.size.x / 2)
+	animation_player.play("move_left")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 func _on_back_options_button_pressed() -> void:
-	camera_2d.position = camera_original_position
+	animation_player.play_backwards("move_left")
